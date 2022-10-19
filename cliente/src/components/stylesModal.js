9da8d6d38ba042e68d8144657f96b200441bf360
Roberto Friber <75671954+FriberNamzug@users.noreply.github.com>
2022-10-19 00:37:24 -0500

    Se crea stylemodal para controlar los estilos de los modales de las acciones

---
+++ cliente/src/components/stylesModal.js
@@ -0,0 +1,10 @@
+export const styleModal = {
+    width: "80%",
+    p: 4,
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    maxHeight: "calc(100% - 64px)",
+    overflowY: "auto",
+};
