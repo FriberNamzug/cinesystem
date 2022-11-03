ad675b7b64c6714aa617933b779abaf0335fc9d9
Roberto Friber <75671954+FriberNamzug@users.noreply.github.com>
2022-11-02 18:51:45 -0600

    Se agregan props a las card para mas dinamismo

--- cliente/src/components/Card.jsx
+++ cliente/src/components/Card.jsx
@@ -2,9 +2,16 @@ import { useState } from 'react'
 import defaultImg from '../assets/img-default.jpg'
 import { Rating, Button } from '@mui/material';
 
-export default function Card() {
+import { useNavigate } from "react-router-dom"
+
+export default function Card({ pelicula = { titulo: "Sin titulo" }, botonTxt = "Ver mas", botonUrl = "#" }) {
 
     const [value, setValue] = useState(2);
+    const navigate = useNavigate()
+
+    const handleClickBtn = () => {
+        navigate(`${botonUrl}`)
+    }
 
     return (
         <div className="rounded-xl bg-white w-64  m-4">
@@ -15,7 +22,7 @@ export default function Card() {
             <div className="flex flex-col w-full justify-center  mt-4">
                 <div className='flex flex-row justify-between items-center px-2'>
                     <span className="pl-2 capitalize">Titulo: </span>
-                    <span className="pl-2 capitalize">Nombre Pelicula</span>
+                    <span className="pl-2 capitalize">{pelicula.titulo}</span>
                 </div>
                 <div className='flex flex-row justify-between items-center px-2'>
                     <span className="pl-2 capitalize">Calificacion:</span>
@@ -27,7 +34,7 @@ export default function Card() {
                         }}
                     />
                 </div>
-                <Button variant="contained" color="success" className="w-full">Ver</Button>
+                <Button variant="contained" color="success" className="w-full" onClick={handleClickBtn}>{botonTxt}</Button>
             </div>
         </div>
     )
