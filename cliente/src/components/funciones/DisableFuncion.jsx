a1403b4e4bfccd415e6a5653319125e558eaad0d
Roberto Friber <75671954+FriberNamzug@users.noreply.github.com>
2022-11-17 04:12:32 -0600

    Se agrega interfaz

--- cliente/src/components/funciones/DisableFuncion.jsx
+++ cliente/src/components/funciones/DisableFuncion.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useState } from "react";
+import {
+    TextField,
+    Button,
+    CircularProgress,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { toast } from "react-toastify";
 
-export default function DisableFuncion() {
+export default function DisableFuncion({ funcion, close }) {
     return (
-        <div>DisableFuncion</div>
+        <div className="flex flex-col  border border-black rounded-lg bg-white ">
+            <div className="text-right">
+                <CloseIcon
+                    onClick={() => close()}
+                    className="cursor-pointer text-red-400 hover:text-red-900 "
+                />
+            </div>
+        </div>
     )
 }
+
+
--- cliente/src/components/funciones/EditFuncion.jsx
+++ cliente/src/components/funciones/EditFuncion.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
 
-export default function EditFuncion() {
+import { useState } from "react";
+import {
+  TextField,
+  Button,
+  CircularProgress,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import { toast } from "react-toastify";
+
+export default function EditFuncion({funcion, close}) {
   return (
-    <div>Edit Funcion</div>
+    <div className="flex flex-col  border border-black rounded-lg bg-white ">
+      <div className="text-right">
+        <CloseIcon
+          onClick={() => close()}
+          className="cursor-pointer text-red-400 hover:text-red-900 "
+        />
+      </div>
+    </div>
   )
 }
